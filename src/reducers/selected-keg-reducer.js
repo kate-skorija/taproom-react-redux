918b77452eed782a1fb15e0e5a5d62352ab202ea
Kate Skorija <kateskorija@gmail.com>
2020-09-18 10:48:45 -0700

    create select keg case to pass test

--- src/reducers/selected-keg-reducer.js
+++ src/reducers/selected-keg-reducer.js
@@ -1,3 +1,16 @@
 export default (state = null, action) => {
-  return state;
+  const { name, brand, price, alcoholContent, pintsRemaining, id } = action;
+  switch(action.type){
+  case 'SELECT_KEG':
+    return {
+      [id]: { name: name,
+        brand: brand,
+        price: price,
+        alcoholContent: alcoholContent,
+        pintsRemaining: pintsRemaining,
+        id: id }
+    }
+    default:
+      return state;
+  }
 };
