76601ae7c81c60759865417c959dc079defd8f55
Kate Skorija <kateskorija@gmail.com>
2020-09-18 10:40:04 -0700

    create and fail test for selecting keg

--- src/__tests__/reducers/selected-keg-reducer.test.js
+++ src/__tests__/reducers/selected-keg-reducer.test.js
@@ -1,9 +1,41 @@
-import selectedPostReducer from '../../reducers/selected-keg-reducer';
+import selectedKegReducer from '../../reducers/selected-keg-reducer';
 
 describe('selectedKegReducer', () => {
-  
+
+  let action;
+  const kegData = {
+    name: 'La Brea Brown',
+    brand: 'Fossil Cove Brewery',
+    price: '$5.00',
+    alcoholContent: '6.0%',
+    pintsRemaining: 124,
+    id: 1
+  };
+
   test('Should return null if no post is selected', () => {
-		expect(selectedPostReducer(null, { type: null })).toEqual(null);
+		expect(selectedKegReducer(null, { type: null })).toEqual(null);
 	});
 
+  test('Should return selected keg', () => {
+    const { name, brand, price, alcoholContent, pintsRemaining, id } = kegData;
+    action = {
+      type: 'SELECT_KEG',
+      name: name,
+      brand: brand,
+      price: price,
+      alcoholContent: alcoholContent,
+      pintsRemaining: pintsRemaining,
+      id: id
+    };
+    expect(selectedKegReducer(null, action)).toEqual({
+      [id] : {
+        name: name,
+        brand: brand,
+        price: price,
+        alcoholContent: alcoholContent,
+        pintsRemaining: pintsRemaining,
+        id: id
+      }
+    })
+  })
 })
