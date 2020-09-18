e38a4c5a5f76686d9ed5967999ba634cfc4db3da
Kate Skorija <kateskorija@gmail.com>
2020-09-18 10:25:54 -0700

    create and fial test to toggle editing form visibility

--- src/__tests__/reducers/editing-reducer.test.js
+++ src/__tests__/reducers/editing-reducer.test.js
@@ -5,4 +5,8 @@ describe('editingReducer', () => {
   test('Should return default state if no action type is recognized', () => {
     expect(editingReducer(false, { type: null })).toEqual(false);
   });
+
+  test('Should toggle editing form visibility state to true', () => {
+    expect(editingReducer(false, { type: 'TOGGLE_FORM' })).toEqual(true);
+  });
 })
