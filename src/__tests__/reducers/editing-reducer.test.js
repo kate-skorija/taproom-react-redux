9b43c12e5eb35fbe1d19b716829604adfd0495fe
Kate Skorija <kateskorija@gmail.com>
2020-09-18 10:25:15 -0700

    pass test for intial state of editing reducer

--- src/__tests__/reducers/editing-reducer.test.js
+++ src/__tests__/reducers/editing-reducer.test.js
@@ -3,6 +3,6 @@ import editingReducer from '../../reducers/editing-reducer';
 describe('editingReducer', () => {
 
   test('Should return default state if no action type is recognized', () => {
-    expect(formVisibleReducer(false, { type: null })).toEqual(false);
+    expect(editingReducer(false, { type: null })).toEqual(false);
   });
 })
