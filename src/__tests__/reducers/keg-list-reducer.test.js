31bb8544e9f436134066fdc97474da2728820e08
Kate Skorija <kateskorija@gmail.com>
2020-09-18 08:23:21 -0700

    create and fail test for adding keg to keg list

--- src/__tests__/reducers/keg-list-reducer.test.js
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -1,7 +1,41 @@
 import kegListReducer from '../../reducers/keg-list-reducer';
 
 describe('kegListReducer', () => {
+
+  let action;
+  const kegData = {
+    name: 'La Brea Brown',
+    brand: 'Fossil Cove Brewery',
+    price: '$5.00',
+    alcoholContent: '6.0%',
+    pintsRemaining: 124,
+    id: 1
+  };
+
   test ('Should return default state if there is no action type passed into the reducer', () => {
     expect(kegListReducer({}, { type: null })).toEqual({});
+  });
+
+  test('Should successfully add new ticket data to master TicketList', () => {
+    const { name, brand, price, alcoholContent, pintsRemaining, id } = kegData;
+    action = {
+      type: 'ADD_KEG',
+      name: name,
+      brand: brand,
+      price: price,
+      alcoholContent: alcoholContent,
+      pintsRemaining: pintsRemaining,
+      id: id
+    };
+    expect(kegListReducer({}, action)).toEqual({
+      [id] : {
+        name: name,
+        brand: brand,
+        price: price,
+        alcoholContent: alcoholContent,
+        pintsRemaining: pintsRemaining,
+        id: id
+      }
+    });
   });
 });
