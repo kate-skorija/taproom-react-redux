1952fc27868873bffb49914b31562d7ae179a907
Kate Skorija <kateskorija@gmail.com>
2020-09-18 13:43:35 -0700

    add actions to debug update functionality

--- src/components/KegControl.js
+++ src/components/KegControl.js
@@ -68,7 +68,11 @@ class KegControl extends React.Component {
   handleEditingKeg = (kegToEdit) => {
     const { dispatch } = this.props;
     const action = a.addKeg(kegToEdit);
+    const action2 = a.toggleEditForm();
+    const action3 = a.deselectKeg();
     dispatch(action);
+    dispatch(action2);
+    dispatch(action3);
   };
 
   render(){
