6edbfdb20a8cd691d3e33840c8cf88a971db5f32
Kate Skorija <kateskorija@gmail.com>
2020-10-01 17:06:01 -0700

    Updates

---
+++ precache-manifest.65babff8596acb94e1a3a8e63c045e0f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcedeb5c42f4179cadc75d35394d99ef",
+    "url": "/taproom-redux/index.html"
+  },
+  {
+    "revision": "f237e096a243dd6b2501",
+    "url": "/taproom-redux/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d6f9590a78fd9bfdc11b",
+    "url": "/taproom-redux/static/js/2.ef35e5c1.chunk.js"
+  },
+  {
+    "revision": "f237e096a243dd6b2501",
+    "url": "/taproom-redux/static/js/main.a3f5fc95.chunk.js"
+  },
+  {
+    "revision": "4a1db5323fa140602c15",
+    "url": "/taproom-redux/static/js/runtime-main.554e76f9.js"
+  },
+  {
+    "revision": "2570c231f12294d3d594a707c03d3f56",
+    "url": "/taproom-redux/static/media/beer.2570c231.jpg"
+  },
+  {
+    "revision": "8713e736ee7903dd1ef2276a46b40055",
+    "url": "/taproom-redux/static/media/fishbowl.8713e736.jpg"
+  }
+]);
